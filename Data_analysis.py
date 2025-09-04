a15beca05ffe3ffc97d5690ea109f1ce8437bf9f
matheolentz <lentz.matheo@gmail.com>
2025-09-04 12:32:59 +0200

    Initialisation

--- Data_analysis.py
+++ Data_analysis.py
@@ -1,6 +1,24 @@
 import pandas as pd
+import matplotlib.pyplot as plt
 
-data = pd.read_csv("/Users/matheolentz/Desktop/Day_ahead_price_history.csv", sep=";")
+df = pd.read_csv("/Users/matheolentz/Desktop/Day_ahead_price_history.csv", sep=";", encoding="utf-8")
+df["datetime"] = pd.to_datetime(df["Start date"], dayfirst=True, errors="coerce")
 
-print(data.head())
-print(data.columns)
+#print(df.head())
+print(df.columns)
+
+data_DELU = df[["datetime", "Germany/Luxembourg [€/MWh] Original resolutions"]]
+data_DELU = data_DELU.rename(columns={"Germany/Luxembourg [€/MWh] Original resolutions": "price_EUR_MWh"})
+print(data_DELU)
+
+
+# Plot time series
+plt.figure(figsize=(15,5))
+plt.plot(data.index, data["price_EUR_MWh"], color="blue", linewidth=1)
+
+plt.title("Germany/Luxembourg Day-Ahead Electricity Prices")
+plt.xlabel("Date")
+plt.ylabel("Price [€/MWh]")
+plt.grid(True)
+plt.tight_layout()
+plt.show()
