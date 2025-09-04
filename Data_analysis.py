b95d88cd69c893e968b9ae6389612dd1a0a2e16c
matheolentz <lentz.matheo@gmail.com>
2025-09-04 20:46:18 +0200

    Initialisation

--- Data_analysis.py
+++ Data_analysis.py
@@ -1,20 +1,24 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
-# Parameters
+#-------------
+# Parameters of the battery
+#-------------
 capacity = 4400 #in MWh
 charge = 1 #in MW
 life_cycle = 10
 capex = 250  #EUR/kW
 Wacc = 0.1
-
+#-------------
+# Load Data
+#-------------
 
 df = pd.read_csv("/Users/matheolentz/Desktop/Day_ahead_price_history.csv", sep=";", encoding="utf-8")
 df["datetime"] = pd.to_datetime(df["Start date"], dayfirst=True, errors="coerce")
 
-#print(df.head())
 print(df.columns)
 
+#Select German prices
 data_DELU = df[["datetime", "Germany/Luxembourg [€/MWh] Original resolutions"]]
 data_DELU = data_DELU.rename(columns={"Germany/Luxembourg [€/MWh] Original resolutions": "price_EUR_MWh"})
 print(data_DELU)
